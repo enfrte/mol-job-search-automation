e3a5514ce6580d2b5a57f2be536196ebd892f611
enfrte <enfrte@gmail.com>
2020-02-26 13:59:58 +0200

    Tidy up and updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MOL Job Search Notification
 
-I wanted to automate my job searching, so that I get new job listings delivered to my email inbox. The mol.fi site calls search results independently of the main page content, so regular scrapers don't do the job. A nice Node app called Puppeteer manages to work its way around this by utilizing a headless chrome build and pulling data from that. On top of this, I used Netlify's lambda functions to create a server-less API for my PHP hosting to query. 
+I wanted to automate my job searching, so that I get new job listings delivered to my email inbox. The mol.fi site calls search results independently of the main page content, so regular scrapers don't do the job. A nice Node app called Puppeteer manages to work its way around this by utilizing a headless chrome build and pulling data from that. 
+
+I'm currently trying to use Netlify's lambda functions to create a server-less API for my PHP hosting to query, but am having some issues. 
+
+For the basic scraper code, check out `/scraper.js`. For the netlify lambda version, check out `/functions/scrapeMol.js`
--- scrapers.js
+++ scrapers.js
@@ -1,32 +1,6 @@
-const fs = require('fs');
-
-const dataBaseUrl = 'mol-results.json';
-
-// read the database
-const dataBaseContents = fs.readFileSync(dataBaseUrl, 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log('dataBaseContents returned', data);
-  return data;
-});
-
-// write to the database
-function writeToDatabase(url, data) {
-  try {
-    fs.writeFile(url, data, 'utf8', function (err) {
-      if (err) {
-          console.log("An error occured while writing JSON Object to File.");
-          return console.log(err);
-      }
-      console.log("JSON file has been updated.");
-    });
-  } catch (error) {
-    console.log(error);
-  }    
-}
-
 // DOCS: https://github.com/puppeteer/puppeteer/blob/master/docs/api.md
 const puppeteer = require('puppeteer');
-const molUrl = 'https://paikat.te-palvelut.fi/tpt/?searchPhrase=web%20developer&locations=Oulu&announced=0&leasing=0&english=false&sort=1'
+const MolUrl = 'https://paikat.te-palvelut.fi/tpt/?searchPhrase=web%20developer&locations=Oulu&announced=0&leasing=0&english=false&sort=1'
 
 // scrape mol.fi 
 async function scrapeMol(url) {
@@ -59,18 +33,4 @@ async function scrapeMol(url) {
     await browser.close();
   }
 }
-scrapeMol(molUrl);
-
-// run a check to see if data is different
-async function check() {
-  const urls = await scrapeMol(url);
-  const stringifyUrls = JSON.stringify(urls);
-  if (dataBaseContents !== stringifyUrls) {
-    writeToDatabase(dataBaseUrl, stringifyUrls)
-    console.log("Database has been updated.\n", stringifyUrls);
-  } else {
-    console.log("There have been no updates since last time.\n", dataBaseContents);
-  }
-}
-
-//check();
+scrapeMol(MolUrl);
